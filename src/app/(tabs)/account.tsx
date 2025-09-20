64c9836a1064c1e050870c5dd6fc985bc0b46e06
Muhammad Usama Ashraf <usamaashraf82@live.com>
2025-09-20 14:04:15 +0500

    update

--- src/app/(tabs)/account.tsx
+++ src/app/(tabs)/account.tsx
@@ -1,24 +1,21 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { Dimensions, Pressable, Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import useUser from '~/store/useUser';
 
-const PAGE_WIDTH = Dimensions.get('window').width;
-const PAGE_HEIGHT = Dimensions.get('window').height;
 const Rental = () => {
   const { user, logout } = useUser();
 
   return (
-    <View
-      id="carousel-component"
-      // dataSet={{ kind: "custom-animations", name: "tinder" }}
-    >
+    <View id="carousel-component">
       {user && (
-        <Pressable
-          onPress={logout}
-          className="mx-6 mt-4 items-center justify-center rounded-md bg-secondary py-3">
-          <Text className="text-lg font-semibold text-white">Logout</Text>
-        </Pressable>
+        <>
+          <Pressable
+            onPress={logout}
+            className="mx-6 mt-4 items-center justify-center rounded-md bg-secondary py-3">
+            <Text className="text-lg font-semibold text-white">Logout</Text>
+          </Pressable>
+        </>
       )}
       {!user && (
         <Link
